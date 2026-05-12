0522fd794f8ff1d8ce313cde891d4ae1ea56a806
Jesus Chacon <jesuszen2.6@gmail.com>
2026-05-12 13:00:35 -0400

    feat: integrate Ant Design Vue and add button component; update configuration and dependencies

---
+++ app/components/field/Button.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+defineProps<{
+  isLoading: boolean;
+  disabled?: boolean;
+}>();
+</script>
+
+<template>
+  <button
+    :disabled="isLoading || disabled"
+    class="btn-primary capitalize disabled:bg-gray-500 disabled:cursor-not-allowed disabled:opacity-80 text-text-footer text-minus-base btn-main"
+  >
+    <LoadingOutlined v-if="isLoading" />
+    <slot v-else />
+  </button>
+</template>
